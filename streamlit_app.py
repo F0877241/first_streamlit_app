27ecbdb8c006144c3e879ebaf7765e6cfe2c16d2
F0877241 <135852377+F0877241@users.noreply.github.com>
2023-06-15 14:45:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,6 @@ streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-
 # normalizeren resultaat json
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # importeren json in dataframe
